90300170354f18ebb2fec04de966cd002e95b36b
roknuzzamanrony25-dev <roknuzzamanrony25@gmail.com>
2026-02-13 18:15:46 +0600

    Final Signal Stabilization Fix

--- server.js
+++ server.js
@@ -4,8 +4,11 @@ const { Server } = require("socket.io");
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
+
 app.use(express.static("public"));
+
 io.on("connection", (socket) => {
+    socket.on("ready", () => socket.broadcast.emit("ready"));
     socket.on("offer", (data) => socket.broadcast.emit("offer", data));
     socket.on("answer", (data) => socket.broadcast.emit("answer", data));
     socket.on("ice", (data) => socket.broadcast.emit("ice", data));
@@ -14,6 +17,6 @@ io.on("connection", (socket) => {
     socket.on("status", (data) => socket.broadcast.emit("status", { ...data, id: socket.id }));
     socket.on("disconnect", () => socket.broadcast.emit("left", socket.id));
 });
-server.listen(process.env.PORT || 3000, () => console.log("Live"));
 
+server.listen(process.env.PORT || 3000, () => console.log("Server Live"));
 
