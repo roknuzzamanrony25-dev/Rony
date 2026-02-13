c343daaf1dce3ecb27bdd490c7c3fd17f7aa76b2
roknuzzamanrony25-dev <roknuzzamanrony25@gmail.com>
2026-02-13 17:35:58 +0600

    Final Dashboard: Light, Sound Slider, WiFi & Battery

--- server.js
+++ server.js
@@ -8,20 +8,20 @@ const io = new Server(server);
 app.use(express.static("public"));
 
 io.on("connection", (socket) => {
-    console.log("New User Connected:", socket.id);
+    console.log("User Connected:", socket.id);
 
     socket.on("ready", () => socket.broadcast.emit("ready"));
 
-    // Signaling Logic
+    // Signaling
     socket.on("offer-front", (data) => socket.broadcast.emit("offer-front", data));
     socket.on("answer-front", (data) => socket.broadcast.emit("answer-front", data));
     socket.on("ice-front", (data) => socket.broadcast.emit("ice-front", data));
 
-    // Remote Commands (Switch & Torch)
+    // Remote Commands
     socket.on("switch-camera", (toId) => io.to(toId).emit("switch-camera"));
     socket.on("toggle-torch", (toId) => io.to(toId).emit("toggle-torch"));
-
-    // Status Updates (Battery, Network, Audio)
+    
+    // Status Updates
     socket.on("status-update", (data) => {
         socket.broadcast.emit("status-update", { ...data, id: socket.id });
     });
