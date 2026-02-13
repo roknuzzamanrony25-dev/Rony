0dc29abb2ed1a8dde4c5d7533cab5a933d387794
roknuzzamanrony25-dev <roknuzzamanrony25@gmail.com>
2026-02-13 18:20:24 +0600

    Ultra Simple Single Monitor Version

--- server.js
+++ server.js
@@ -4,18 +4,13 @@ const { Server } = require("socket.io");
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
-
 app.use(express.static("public"));
 
 io.on("connection", (socket) => {
-    socket.on("ready", () => socket.broadcast.emit("ready"));
     socket.on("offer", (data) => socket.broadcast.emit("offer", data));
     socket.on("answer", (data) => socket.broadcast.emit("answer", data));
     socket.on("ice", (data) => socket.broadcast.emit("ice", data));
-    socket.on("switch", (id) => io.to(id).emit("switch"));
-    socket.on("torch", (id) => io.to(id).emit("torch"));
-    socket.on("status", (data) => socket.broadcast.emit("status", { ...data, id: socket.id }));
-    socket.on("disconnect", () => socket.broadcast.emit("left", socket.id));
+    socket.on("switch", () => socket.broadcast.emit("switch")); // Single switch signal
 });
 
 server.listen(process.env.PORT || 3000, () => console.log("Server Live"));
