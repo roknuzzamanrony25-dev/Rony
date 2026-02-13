4d110fcdd956399b5bb53fb6a6e7b9f3a0650ed6
roknuzzamanrony25-dev <roknuzzamanrony25@gmail.com>
2026-02-13 18:01:27 +0600

    Added Night Vision and Brightness Boost

--- server.js
+++ server.js
@@ -12,16 +12,9 @@ io.on("connection", (socket) => {
     socket.on("offer", (data) => socket.broadcast.emit("offer", data));
     socket.on("answer", (data) => socket.broadcast.emit("answer", data));
     socket.on("ice", (data) => socket.broadcast.emit("ice", data));
-    
-    // Remote Commands
     socket.on("switch", (id) => io.to(id).emit("switch"));
     socket.on("torch", (id) => io.to(id).emit("torch"));
-    
-    // Status Updates (Light, Battery, Network)
-    socket.on("status-update", (data) => {
-        socket.broadcast.emit("status-update", { ...data, id: socket.id });
-    });
-
+    socket.on("status-update", (data) => socket.broadcast.emit("status-update", { ...data, id: socket.id }));
     socket.on("disconnect", () => socket.broadcast.emit("user-left", socket.id));
 });
 
