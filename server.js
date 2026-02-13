448557620b200bb21411ab35f60ec544b6d8f16d
roknuzzamanrony25-dev <roknuzzamanrony25@gmail.com>
2026-02-13 18:10:35 +0600

    Final Super Stable Build

--- server.js
+++ server.js
@@ -4,19 +4,16 @@ const { Server } = require("socket.io");
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
-
 app.use(express.static("public"));
-
 io.on("connection", (socket) => {
-    socket.on("ready", () => socket.broadcast.emit("ready"));
     socket.on("offer", (data) => socket.broadcast.emit("offer", data));
     socket.on("answer", (data) => socket.broadcast.emit("answer", data));
     socket.on("ice", (data) => socket.broadcast.emit("ice", data));
     socket.on("switch", (id) => io.to(id).emit("switch"));
     socket.on("torch", (id) => io.to(id).emit("torch"));
-    socket.on("status-update", (data) => socket.broadcast.emit("status-update", { ...data, id: socket.id }));
-    socket.on("disconnect", () => socket.broadcast.emit("user-left", socket.id));
+    socket.on("status", (data) => socket.broadcast.emit("status", { ...data, id: socket.id }));
+    socket.on("disconnect", () => socket.broadcast.emit("left", socket.id));
 });
+server.listen(process.env.PORT || 3000, () => console.log("Live"));
 
-server.listen(process.env.PORT || 3000, () => console.log("Server Live"));
 
