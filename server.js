cad56019f691eeded467c9ca328121d43b6c8686
roknuzzamanrony25-dev <roknuzzamanrony25@gmail.com>
2026-02-14 17:02:49 +0600

    Final Final Stable Video Fix

--- server.js
+++ server.js
@@ -4,25 +4,16 @@ const { Server } = require("socket.io");
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
-
 app.use(express.static("public"));
 
 io.on("connection", (socket) => {
     socket.on("offer", (data) => socket.broadcast.emit("offer", data));
     socket.on("answer", (data) => socket.broadcast.emit("answer", data));
     socket.on("ice", (data) => socket.broadcast.emit("ice", data));
-    
-    // Remote Commands
     socket.on("switch", (id) => io.to(id).emit("switch"));
     socket.on("torch", (id) => io.to(id).emit("torch"));
-    
-    // Two-Way Status Sync (Light & Battery)
-    socket.on("status-update", (data) => {
-        socket.broadcast.emit("status-update", { ...data, id: socket.id });
-    });
-
+    socket.on("status", (data) => socket.broadcast.emit("status", { ...data, id: socket.id }));
     socket.on("disconnect", () => socket.broadcast.emit("user-left", socket.id));
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log("Server Live"));
+server.listen(process.env.PORT || 3000, () => console.log("Server Live"));
