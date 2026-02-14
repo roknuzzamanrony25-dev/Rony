f7fe472c0a2a52804169056631375fc67c024ffd
roknuzzamanrony25-dev <roknuzzamanrony25@gmail.com>
2026-02-15 00:51:54 +0600

    Final APK WebRTC Fix

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const http = require("http");
 const { Server } = require("socket.io");
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, { cors: { origin: "*" } });
+
 app.use(express.static("public"));
 
 io.on("connection", (socket) => {
@@ -11,4 +12,5 @@ io.on("connection", (socket) => {
     socket.on("answer", (data) => socket.broadcast.emit("answer", data));
     socket.on("ice", (data) => socket.broadcast.emit("ice", data));
 });
+
 server.listen(process.env.PORT || 3000, () => console.log("Server Live"));
